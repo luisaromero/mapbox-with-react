def91ed6374a630dfa26834ffa0531041b8ec263
luisaromero <luisa.marcela.romero@gmail.com>
2019-08-20 12:01:49 -0400

    agregando marcadores a los stores del mapa

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 
-import React, {Component} from 'react';
+import React, {Component , useState} from 'react';
 import MapGL, {NavigationControl , Marker,Popup} from 'react-map-gl';
-// import * as store from './store_directory.json'
-// const store = require('./store_directory.json');
+import  store from './store_directory.json'
+
 
 
 const TOKEN='pk.eyJ1IjoibHVpc2Fyb21lcm8iLCJhIjoiY2p6aGV5MXVpMHJ1ZTNucGhwcng3Y3N0OSJ9.BYd9a8Gh3nCUjwcpQF8scA'; 
 
 class App extends Component {
-  constructor(props) {
-      super(props);
+  constructor() {
+      super();
       this.state = {
         viewport: {
           latitude: 19.432608,  
@@ -19,27 +19,42 @@ class App extends Component {
           pitch: 0,
           width: '100%',
           height: 500,
+         favorite: []
+        } 
+      } ;
+    } 
+    addFavorite(store) {
+          alert('agregado a favoritos')
         }
-      };
-    }
+      
       render() {
+        
           const {viewport} = this.state;
       return (
             <MapGL
-            
+           
               {...viewport}
+             
               onViewportChange={(viewport) => this.setState({viewport})}
               mapStyle="mapbox://styles/luisaromero/cjzhof347397k1cs2nee373nj"
               mapboxApiAccessToken={TOKEN}>
-              <div className="nav" >
-             
-                <NavigationControl onViewportChange={(viewport) => this.setState({viewport})}/>
-    
-       </div>
-    
+               <div className="nav" >
+          <NavigationControl onViewportChange={(viewport) => this.setState({viewport})}/> </div>
+          {store.map(park => ( <Marker latitude={park.Coordinates.lat} longitude={park.Coordinates.lng}>
+            <button className="btn" onClick={
+             this.addFavorite.bind(this)}>
+             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVvf5xv5YL5JUmVR4j9b3HCuBJ7URH5pKTsJx_pkrk9zcsWn9s"></img>
+              </button>
+          </Marker>
+
+          ))} 
       </MapGL>
     );
   }
 }
 
        export default App
+
+
+
+         {/*  */}
