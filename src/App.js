c31121bf21008d47b840f8530d4e0a3f9b3ac9f9
luisaromero <luisa.marcela.romero@gmail.com>
2019-12-16 23:31:01 +0100

    agregando imagen del marcador

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import  store from './store_directory.json'
 
 
 
+
 const TOKEN='pk.eyJ1IjoibHVpc2Fyb21lcm8iLCJhIjoiY2p6aGV5MXVpMHJ1ZTNucGhwcng3Y3N0OSJ9.BYd9a8Gh3nCUjwcpQF8scA'; 
 
 class App extends Component {
@@ -48,18 +49,15 @@ console.log(newList)
           const {viewport} = this.state;
       return (
             <MapGL
-           
               {...viewport}
-             
               onViewportChange={(viewport) => this.setState({viewport})}
               mapStyle="mapbox://styles/luisaromero/cjzhof347397k1cs2nee373nj"
-              mapboxApiAccessToken={TOKEN}
-              >
-               
+              mapboxApiAccessToken={TOKEN}  >
+       
           {store.map(ele => ( <Marker latitude={ele.Coordinates.lat} longitude={ele.Coordinates.lng}>
             <button className= "btn" name ={ele.Name}  onClick={(e) =>
              this.addFavorite(e  ,this , this.name)}>
-               <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVvf5xv5YL5JUmVR4j9b3HCuBJ7URH5pKTsJx_pkrk9zcsWn9s"></img>
+               <img src="https://images.vexels.com/media/users/3/154655/isolated/preview/71dccbb077597dea55dfc5b7a7af52c4-ubicaci--n-pin-icono-de-contacto-by-vexels.png" alt="icono"></img>
               </button>
           </Marker>
 
